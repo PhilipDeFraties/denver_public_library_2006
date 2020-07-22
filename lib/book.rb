f42e4618c43d2aaacfc0b436a8cddd9338dd0b2f
PhilipDeFraties <philipjfry338@gmail.com>
2020-07-22 12:50:06 -0600

    Add attributes reader, instance variables, test.

--- lib/book.rb
+++ lib/book.rb
@@ -1,6 +1,14 @@
 class Book
-
-  def initialize
+  attr_reader :author_first_name,
+              :author_last_name,
+              :title,
+              :publication_date
+              
+  def initialize(info)
+    @author_first_name = info[:author_first_name]
+    @author_last_name = info[:author_last_name]
+    @title = info[:title]
+    @publication_date = info[:publication_date]
   end
 
 end
