fd09f879a6b0ff9e2889cb956ba14c90a733f636
PhilipDeFraties <philipjfry338@gmail.com>
2020-07-22 13:55:41 -0600

    Add books to attr_reader, inst var, and test.

--- test/library_test.rb
+++ test/library_test.rb
@@ -18,4 +18,9 @@ class LibraryTest < Minitest::Test
     assert_equal "Denver Public Library", dpl.name
   end
 
+  def test_it_has_no_books_by_default
+    dpl = Library.new("Denver Public Library")
+
+    assert_equal [], dpl.books
+  end
 end
