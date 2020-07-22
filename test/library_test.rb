0dc44aaeba2f2311e8f2da92c41d73c8d395d4c9
PhilipDeFraties <philipjfry338@gmail.com>
2020-07-22 13:52:34 -0600

    Add library_test.rb and library.rb.

---
+++ test/library_test.rb
@@ -0,0 +1,15 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/author'
+require './lib/library'
+require 'pry'
+
+class LibraryTest < Minitest::Test
+
+  def test_it_exists
+    dpl = Library.new("Denver Public Library")
+
+    assert_instance_of Library, dpl
+  end
+  
+end
